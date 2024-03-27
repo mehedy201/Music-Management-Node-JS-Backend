aa5fff5a0d493ca4564c5de4be8703d386307f98
mehedy201 <mehedy643@gmail.com>
2024-03-28 04:06:52 +0600

    creaed new api for admin now admin can see all withdrawal request and can filter by status and can search by ID

--- controlar/AdminUserBothController/sendPaymentFromDreamRecord.controller.js
+++ controlar/AdminUserBothController/sendPaymentFromDreamRecord.controller.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const { getDb } = require("../../utilities/dbConnect");
 
 // Stored Payment Details___________________________________________________
@@ -17,7 +18,7 @@ module.exports.getPaymentDetails = async (req, res, next) => {
     try {
         const db = getDb();
         // Fint Payment Details under User ______
-        const masterUserId = req.params.id;
+        const masterUserId = req.params.masterUserId;
         const find = await db.collection('stored-pay-details').find({ masterUserId: masterUserId }).toArray();
         const organizeData = find.reverse();
         const dataCount = organizeData.length;
@@ -38,7 +39,7 @@ module.exports.searchPaymentDetailsByYear = async (req, res, next) => {
     try {
         const db = getDb();
         // Fint Payment Details under User ___________
-        const masterUserId = req.params.id;
+        const masterUserId = req.params.masterUserId;
         const searchText = req.query.search;
         const find = await db.collection('stored-pay-details').find({ masterUserId: masterUserId }).toArray();
         const organizeData = find.reverse();
@@ -62,6 +63,112 @@ module.exports.storedWithdrawalDetails = async (req, res, next) => {
         delete withdrawalDetails._id;
         const result = await db.collection('stored-withdrawal-details').insertOne(withdrawalDetails);
         res.send({status: 200, message: 'Successfully Stored Withdrawal Information', data: result});
+    } catch (error) {
+        next(error)
+    }
+}
+
+// Only For Admin API  Start_____________________________________________________________________________
+// Get All Withdrawal Request____________________________________________________________________________
+module.exports.withdrawalReqList = async (req, res, next) => {
+    try {
+        const query ={};
+        const db = getDb();
+        const find = await db.collection('stored-withdrawal-details').find(query).toArray();
+        const status = req.query.status;
+        let organizeData;
+        if(status === 'All'){
+            organizeData = find.reverse()
+        }else{
+            const findByStatus = find.filter(d =>d.status.toLowerCase().includes(status.toLowerCase()));
+            organizeData = findByStatus.reverse();
+        }
+        const dataCount = organizeData.length;
+        // Pagination __________________________
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const data = organizeData.slice(startIndex, endIndex);
+        res.send({status: 200, message: 'Sucessfull get All withdrawal Details', data: data, dataCount: dataCount});
+    } catch (error) {
+        next(error)
+    }
+}
+// Withdrawal Search By ID
+module.exports.getSingleWithdrawalDetailsBySearch = async (req, res, next) => {
+    try {
+        const query ={};
+        const db = getDb();
+        const find = await db.collection('stored-withdrawal-details').find(query).toArray();
+        const status = req.query.status;
+        // Filter Labels by labelsName_________
+        const searchText = req.query.search;
+        console.log(status);
+        let data;
+        if(status === 'All'){
+            if(searchText){
+                const findById = find.filter(d =>d._id == searchText)
+                data = findById.reverse();
+            }else{
+                data = find.reverse();
+            }
+        }else{
+            if(searchText){
+                const findByStatus = find.filter(d =>d.status.toLowerCase().includes(status.toLowerCase()));
+                const findById = findByStatus.filter(d =>d._id == searchText);
+                data = findById.reverse();
+            }else{
+                const findByStatus = find.filter(d =>d.status.toLowerCase().includes(status.toLowerCase()));
+                data = findByStatus.reverse();
+            }
+        }
+        const dataCount = data.length;
+        res.send({status: 200, message: 'Successfully Get labels List by Search', data: data, dataCount: dataCount});
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// Only For Admin API  End_____________________________________________________________________________
+// ____________________________________________________________________________________________________
+
+
+// Get Withdrawal details data under the User_________________________________
+module.exports.getWithdrawalDetails = async (req, res, next) => {
+    try {
+        const db = getDb();
+        // Fint Payment Details under User ______
+        const masterUserId = req.params.masterUserId;
+        const find = await db.collection('stored-withdrawal-details').find({ masterUserId: masterUserId }).toArray();
+        const organizeData = find.reverse();
+        const dataCount = organizeData.length;
+        // Pagination __________________________
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const data = organizeData.slice(startIndex, endIndex);
+        res.send({status: 200, message: 'Sucessfull get withdrawal Details under The user', data: data, dataCount: dataCount});
+    } catch (error) {
+        next(error)
+    }
+}
+
+// Search Withdrawal Details by Year_________________________________
+module.exports.searchWithdrawalDetailsByYear = async (req, res, next) => {
+    try {
+        const db = getDb();
+        // Fint Payment Details under User ___________
+        const masterUserId = req.params.masterUserId;
+        const searchText = req.query.search;
+        const find = await db.collection('stored-withdrawal-details').find({ masterUserId: masterUserId }).toArray();
+        const organizeData = find.reverse();
+        // Search Data By Year______________________
+        const searchData = organizeData.filter(d => d.withdrawalYear == searchText);
+        const dataCount = searchData.length;
+        res.send({status: 200, message: 'Successfully Search withdrawal Details By Year', data: searchData, dataCount: dataCount});
     } catch (error) {
         next(error)
     }
--- routes/v1/AdminUserBothRoute/sendPaymentFromDreamRecord.route.js
+++ routes/v1/AdminUserBothRoute/sendPaymentFromDreamRecord.route.js
@@ -4,11 +4,17 @@ const router = express.Router();
 
 
 router.post('/', sendPaymentFromDreamRecord.storedPaymentDetails);
-router.get('/:id', sendPaymentFromDreamRecord.getPaymentDetails);
-router.get('/search/:id', sendPaymentFromDreamRecord.searchPaymentDetailsByYear);
+router.get('/:masterUserId', sendPaymentFromDreamRecord.getPaymentDetails);
+router.get('/search/:masterUserId', sendPaymentFromDreamRecord.searchPaymentDetailsByYear);
 
 router.post('/withdrawal/', sendPaymentFromDreamRecord.storedWithdrawalDetails);
+router.get('/withdrawal/:masterUserId', sendPaymentFromDreamRecord.getWithdrawalDetails);
+router.get('/withdrawal/search/:masterUserId', sendPaymentFromDreamRecord.searchWithdrawalDetailsByYear);
 
+// Only For Admin Route _________________________________________________________________________________
+
+router.get('/admin/withdrawal/req-list', sendPaymentFromDreamRecord.withdrawalReqList);
+router.get('/admin/withdrawal/search-req-list', sendPaymentFromDreamRecord.getSingleWithdrawalDetailsBySearch);
 
 
 module.exports = router;
